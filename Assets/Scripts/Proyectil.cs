2dd3c176543386ccd2ca7958011456366ecd1968
DielopAlf <114227973+DielopAlf@users.noreply.github.com>
2023-09-19 13:21:35 +0100

    SCRIPT

--- Assets/Scripts/Proyectil.cs
+++ Assets/Scripts/Proyectil.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Proyectil : MonoBehaviour
 {
+    public float Velocidad;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        transform.position += new Vector3(0, Velocidad * Time.deltaTime, 0);
+
     }
+    private void OncollicionEnter2D (Collision2D collision)
+    {
+
+        if(collision.gameObject.name == "enemy")
+        {
+
+            collision.gameObject.GetComponentInParent<NaveEnemiga>().Vida -= 1;
+
+        }
+
+    }
+
+
+
 }
